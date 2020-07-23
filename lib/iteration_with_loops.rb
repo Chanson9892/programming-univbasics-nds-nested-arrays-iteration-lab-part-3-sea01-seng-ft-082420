ae85a5bc5ca0414e16eb31cbde0ba5c374905dc0
Chandler Hanson <chan.hanson5@gmail.com>
2020-07-23 00:38:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,3 @@
-require pry
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
@@ -15,5 +14,4 @@ def join_nested_strings(src)
   row_index += 1
   end
   string
-  binding.pry
 end
